158f6b0236edd9e757a8115f40c676fcb67357d6
muhoed <dargunov@yahoo.com>
2021-12-04 23:07:44 +0100

    test forms - in-progress

--- commerce/auctions/tests/test_forms.py
+++ commerce/auctions/tests/test_forms.py
@@ -1,7 +1,32 @@
 from django.test import TestCase
+from django.http import HttpRequest
 
 from auctions.forms import *
+from auctions.models import *
 
 
-class LoginFormTest(TestCase):
-	pass
+class RegisterFormTest(TestCase):
+    """
+    Testing RigisterForm core functionality.
+    """
+
+    def setUp(self):
+        self.form = RegisterForm()
+
+    def test_empty_form(self):
+        self.assertIn("email", self.form.fields)
+        
+    def test_post_data_to_form(self):
+        request = HttpRequest()
+        request.POST = {
+            'title': 'MR',
+            'first_name': 'User',
+            'last_name': 'Testoff',
+            'email': 'test@email.test'
+        }
+        
+        form = RegisterForm(request.POST)
+        form.save()
+        self.assertEqual(User.objects.count(), 1)
+		
+	
