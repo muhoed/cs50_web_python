bf9cc841180c3469e246496b0db1b4142005f0f4
Dmitry Argunov <dargunov@yahoo.com>
2022-01-09 22:40:37 +0100

    finish compose_email

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -11,22 +11,49 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 function compose_email() {
+  
+  // Handle error if any
+  let err_msg = null;
+
+  function handleSendEmailError(error) {
+    if (err_msg === null) {
+      err_msg = document.createElement('p');
+      err_msg.style.color = 'red';
+      err_msg.style.fontWeight= 'bold';
+      document.querySelector('#compose-view').insertBefore(err_msg, document.querySelector('form'));
+    }
+    err_msg.innerHTML = error;
+    window.scrollTo(0, 0);
+  }
 
   // Show compose view and hide other views
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
+  const emails_view = document.querySelector('#emails-view');
+  const compose_view = document.querySelector('#compose-view');
+  emails_view.style.display = 'none';
+  compose_view.style.display = 'block';
 
   // Clear out composition fields
-  let recipients = document.querySelector('#compose-recipients');
-  let subject = document.querySelector('#compose-subject');
-  let body = document.querySelector('#compose-body');
+  const recipients = document.querySelector('#compose-recipients');
+  const subject = document.querySelector('#compose-subject');
+  const body = document.querySelector('#compose-body');
   
   recipients.value = '';
   subject.value = '';
   body.value = '';
 
   // Send email on submit button click
-  document.querySelector('form').onsubmit = function send_email () {
+  document.querySelector('form').onsubmit = () => {
+
+    // Validate user input
+    if (subject.value === '' || /\s+/.test(subject.value)) {
+      handleSendEmailError('Please add some email subject.');
+      return false;
+    } else if (body.value === '' || /\s+/.test(body.value)) {
+      handleSendEmailError('Please add some email content.');
+      return false;
+    }
+
+    // Send email
     fetch('/emails', {
       method: 'POST',
       body: JSON.stringify({
@@ -37,24 +64,19 @@ function compose_email() {
     })
     .then(response => response.json())
     .then(result => {
-        // Print result
-        console.log(result);
         if (!result.error) {
           console.log('Email was sent.');
           load_mailbox('sent');
         } else {
-          let err_msg = document.createElement('p');
-          err_msg.innerHTML = result.error;
-          err_msg.style.color = 'red';
-          err_msg.style.fontWeight= 'bold';
-          document.querySelector('#compose-view').insertBefore(err_msg, document.querySelector('form'));
-          window.scrollTo(0, 0);
+          handleSendEmailError(result.error);
         }
     })
     .catch(error => {
       // Print error
-      console.log(error);
+      console.log('Fetch data error: ' + error);
     });
+
+    // Prevent default onsubmit form behaviour
     return false;
   };
 }
