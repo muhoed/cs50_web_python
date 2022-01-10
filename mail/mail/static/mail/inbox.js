b98acaf48d8542c5a03dbee7824f079d224a09cf
Dmitry Argunov <dargunov@yahoo.com>
2022-01-10 23:40:55 +0100

    mailbox view - wip

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -27,9 +27,8 @@ function compose_email() {
   }
 
   // Show compose view and hide other views
-  const emails_view = document.querySelector('#emails-view');
+  document.querySelector('#emails-view').style.display = 'none';
   const compose_view = document.querySelector('#compose-view');
-  emails_view.style.display = 'none';
   compose_view.style.display = 'block';
 
   // Clear out composition fields
@@ -76,17 +75,93 @@ function compose_email() {
       console.log('Fetch data error: ' + error);
     });
 
-    // Prevent default onsubmit form behaviour
+    // Prevent default submit form behaviour
     return false;
   };
 }
 
 function load_mailbox(mailbox) {
+
+  let err_msg;
   
   // Show the mailbox and hide other views
-  document.querySelector('#emails-view').style.display = 'block';
+  const emails_view = document.querySelector('#emails-view');
+  emails_view.style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  // Load mailbox
+  fetch('/emails/' + mailbox)
+  .then(response => response.json())
+  .then(emails => {
+    if (emails.error) {
+      err_msg = document.createElement('p');
+      err_msg.style.fontWeight = 'bold';
+      err_msg.style.color = 'red';
+      err_msg.innerHTML('Error: ' + emails.error);
+      emails_view.append(err_msg);
+    } else {
+      let sent = (mailbox === 'sent') ? true : false;
+      emails_view.append(get_header(sent));
+      emails.forEach(email => {
+        emails_view.append(get_msg(email, sent));
+      })
+    }
+  })
+
+}
+
+function get_header(sent) {
+  // create header for mailbox views
+  const header = document.createElement('div');
+  header.classList += 'row w-100 m-2';
+
+  const hfrom = document.createElement('div');
+  hfrom.classList += 'col-4 p-1';
+  hfrom.innerHTML = (sent) ? 'To' : 'From';
+
+  const hsubject = document.createElement('div');
+  hsubject.classList += 'col-5 p-1';
+  hsubject.innerHTML = 'Subject';
+
+  const hdt = document.createElement('div');
+  hdt.classList += 'col-3 p-1';
+  hdt.innerHTML = 'Time';
+
+  header.append(...[hfrom, hsubject, hdt]);
+
+  return header;
+}
+
+function get_msg(email, sent) {
+  // message container
+  msg = document.createElement('div');
+  msg.classList += 'row w-100 border border-top-1 border-bottom-1 border-left-1 border-right-1 m-2';
+  (email.read) ? msg.classList += 'bg bg-light' : msg.classList += '';
+  msg.onmouseover = () => {msg.style.opacity = '0.7'; msg.style.cursor = 'pointer'};
+  msg.onmouseout = () => {msg.style.opacity = '1.0'};
+  msg.addEventListener('click', () => load_email(email.id));
+  // 'from' column
+  from = document.createElement('div');
+  from.classList += 'col-4 p-1';
+  from.innerHTML = (sent) ? email.recipient : email.sender;
+  // 'subject' column
+  subject = document.createElement('div');
+  subject.classList += 'col-5 p-1';
+  subject.innerHTML = email.subject;
+  // timestamp column
+  dt = document.createElement('div');
+  dt.classList += 'col-3 p-1';
+  dt.innerHTML = email.timestamp;
+  // create row
+  msg.append(...[from, subject, dt]);
+
+  return msg;
+}
+
+function load_email(id) {
+  // TODO
+  return false;
 }
